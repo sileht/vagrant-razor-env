5169f323719a21ef53bf55d699dbe50ca5b93278
Mehdi Abaakouk <sileht@sileht.net>
2012-08-29 09:28:14 +0200

    Add dev repo

--- deploy_post_env.sh
+++ deploy_post_env.sh
@@ -6,9 +6,21 @@ patch -p1  < /vagrant/preseed.local_repo.patch # < not in wiki this my debian re
 
 gem install rspec
 gem install rspec-puppet
+gem install rake
 
-git clone git://github.com/puppetlabs/puppetlabs_spec_helper.git
-cd puppetlabs_spec_helper
+git clone git://github.com/puppetlabs/puppetlabs_spec_helper.git /root/puppetlabs_spec_helper
+cd /root/puppetlabs_spec_helper
 rake package:gem
 gem install pkg/puppetlabs_spec_helper-*.gem
+
+
+cd /etc/puppet/modules/razor
+git remote add sileht git@github.com:sileht/puppetlabs-razor.git
+#git fetch sileht
+
+cd /opt/razor/
+git remote add sileht git@github.com:sileht/Razor.git
+git remote add upstream http://github.com/puppetlabs/Razor.git
+#git fetch sileht
+git fetch upstream
 
